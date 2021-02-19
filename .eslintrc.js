7e6b5cec9a88fd357975b007353bbbe0c2fe4c83
malekitoon <malekitoon@yahoo.com>
2021-02-19 15:26:46 +0200

    broken our homepage into smaller components, finished styling menu-item component

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'jsx-quotes': ['error', 'prefer-single'],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'object-curly-newline': ['error', { multiline: true }],
+    'arrow-parens': ['error', 'as-needed'],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'react/no-unescaped-entities': 0,
+    'no-shadow': 0,
+    'react/jsx-props-no-spreading': 0,
+    'no-underscore-dangle': 0,
+    'jsx-a11y/control-has-associated-label': 0,
+    'no-use-before-define': 'off',
+    'jsx-a11y/label-has-associated-control': 0,
+    'consistent-return': 0,
+    'class-methods-use-this': 0,
+    'func-names': 0,
+    'react/prop-types': 0,
+  },
+};
--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import './App.css';
-import Homepage from './homepage.component';
+import Homepage from './pages/homepage/homepage.component';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Homepage />
     </div>
   );
